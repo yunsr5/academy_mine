dd323518b331a2f57315dfc2f77178914731b136
goott3-1s <cjs02272002@gmail.com>
2025-09-02 13:14:08 +0900

    first commit

---
+++ src/test/java/com/ac/kr/academy/AcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.kr.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
